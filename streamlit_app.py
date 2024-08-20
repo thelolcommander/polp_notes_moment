80f214184eb5e1659f35e4c291c3af9ad5e4d91f
thelolcommander <48478823+thelolcommander@users.noreply.github.com>
2024-08-20 16:51:10 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,6 @@ if uploaded_file is not None:
 
     try:
         response = chat_session.send_message(extracted_text)
-        st.markdown("Generated Notes:\n",response.text, unsafe_allow_html=True)
+        st.text_area("Generated Notes:", value=response.text, height=400, disabled=True)
     except Exception as e:
         st.error(f"Error while processing text with AI: {e}")
